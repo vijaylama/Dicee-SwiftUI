85e382b86b956afe8715a9e94f12c55ae12068ce
Vijay Lama <vijaylama2010@gmail.com>
2021-03-24 10:35:59 -0400

    Initial Commit

---
+++ Dicee-SwifUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dicee-SwifUI
+//
+//  Created by Vijay Lama on 3/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
